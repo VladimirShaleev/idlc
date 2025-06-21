e74c78db52a55ae119c0770b00ce42d3b82e71af
Vladimir Shaleev <vladimirshaleev@gmail.com>
2025-06-21 13:01:25 +0300

    Update doc generator for C

--- src/generator_c.cpp
+++ src/generator_c.cpp
@@ -45,6 +45,12 @@ struct DocRef : Visitor {
         str += name.str;
     }
 
+    void visit(ASTMethod* node) override {
+        CName name;
+        node->accept(name);
+        str = "::" + name.str;
+    }
+
     void discarded(ASTNode* node) override {
         CName name;
         node->accept(name);

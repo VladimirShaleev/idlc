61b8f2c87ceab0e6669e3d3166ea435cbd1d1ac0
Vladimir Shaleev <vladimirshaleev@gmail.com>
2025-07-03 14:02:46 +0300

    Update to 1.5.2

--- include/idlc/idl.h
+++ include/idlc/idl.h
@@ -58,7 +58,7 @@ idl_version(void);
 
 /**
  * @brief   Current library version as human-readable string.
- * @details Format: "major.minor.micro", eg: "1.5.1".
+ * @details Format: "major.minor.micro", eg: "1.5.2".
  * @return  Return version string.
  */
 idl_api idl_utf8_t

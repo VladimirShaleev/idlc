e5f06bcdfc9108ea533314a08d988779fd0f1785
Vladimir Shaleev <vladimirshaleev@gmail.com>
2025-07-04 17:18:21 +0300

    Update to 1.5.5

--- include/idlc/idl-version.h
+++ include/idlc/idl-version.h
@@ -39,7 +39,7 @@
  * @sa    IDL_VERSION
  * @sa    IDL_VERSION_STRING
  */
-#define IDL_VERSION_MICRO 4
+#define IDL_VERSION_MICRO 5
 
 /** @} */
 
@@ -65,7 +65,7 @@
 
 /**
  * @brief     Internal macro for string version generation
- * @details   Helper macro that stringizes version components (e.g., 1, 5, 4 -> "1.5.4")
+ * @details   Helper macro that stringizes version components (e.g., 1, 5, 5 -> "1.5.5")
  * @param[in] major Major version number
  * @param[in] minor Minor version number
  * @param[in] micro Micro version number
@@ -78,7 +78,7 @@
 /**
  * @def       IDL_VERSION_STRINGIZE
  * @brief     Creates version string from components
- * @details   Generates a string literal from version components (e.g., 1, 5, 4 -> "1.5.4")
+ * @details   Generates a string literal from version components (e.g., 1, 5, 5 -> "1.5.5")
  * @param[in] major Major version number
  * @param[in] minor Minor version number
  * @param[in] micro Micro version number
@@ -109,7 +109,7 @@
 /**
  * @def     IDL_VERSION_STRING
  * @brief   Library version as human-readable string
- * @details Version string in "MAJOR.MINOR.MICRO" format (e.g., "1.5.4").
+ * @details Version string in "MAJOR.MINOR.MICRO" format (e.g., "1.5.5").
  *          Use #IDL_VERSION for numeric comparisons.
  * @sa      IDL_VERSION
  */
--- include/idlc/idl.h
+++ include/idlc/idl.h
@@ -58,7 +58,7 @@ idl_version(void);
 
 /**
  * @brief   Current library version as human-readable string.
- * @details Format: "major.minor.micro", eg: "1.5.4".
+ * @details Format: "major.minor.micro", eg: "1.5.5".
  * @return  Return version string.
  */
 idl_api idl_utf8_t

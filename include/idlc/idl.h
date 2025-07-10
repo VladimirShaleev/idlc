983a321712e37a26126bf625618b0bdc88796e63
Vladimir Shaleev <vladimirshaleev@gmail.com>
2025-07-10 19:37:53 +0300

    Add idlc.esm.d.ts

--- include/idlc/idl.h
+++ include/idlc/idl.h
@@ -63,7 +63,7 @@ idl_version(void);
 
 /**
  * @brief   Current library version as human-readable string.
- * @details Format: "major.minor.micro", eg: "1.5.11".
+ * @details Format: "major.minor.micro", eg: "1.5.12".
  * @return  Return version string.
  * @ingroup functions
  */

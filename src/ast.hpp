7f06408208bcb56fc0aa06a5d5055e92239c725f
Vladimir Shaleev <vladimirshaleev@gmail.com>
2026-07-21 20:31:55 +0300

    Fix const IDL_AST_NODE_TYPE_DECL_PREV_SIBLING_REF

--- src/ast.hpp
+++ src/ast.hpp
@@ -10,7 +10,8 @@ typedef idl_ast_node_h ASTNodeHandle;
 typedef idl_ast_node_type_t ASTNodeType;
 typedef idl_ast_node_state_flags_t ASTNodeFlags;
 
-static constexpr ASTNodeType IDL_AST_NODE_TYPE_DECL_PREV_SIBLING_REF = ASTNodeType(IDL_AST_NODE_TYPE_MAX_ENUM - 1);
+static constexpr ASTNodeType IDL_AST_NODE_TYPE_PRIVATE               = ASTNodeType(uint8_t(IDL_AST_NODE_TYPE_FLOAT_64 + 1));
+static constexpr ASTNodeType IDL_AST_NODE_TYPE_DECL_PREV_SIBLING_REF = ASTNodeType(uint8_t(IDL_AST_NODE_TYPE_PRIVATE + 0));
 
 static constexpr ASTNodeHandle HandleNone{ 0xFFFF };
 
@@ -66,28 +67,29 @@ struct ASTNode {
 
 inline bool isNodeType(const ASTNode* node, ASTNodeType type) noexcept {
     if (node) {
-        if (node->type == type) {
+        const auto nodeType = node->type;
+        if (nodeType == type) {
             return true;
         }
         switch (type) {
             case IDL_AST_NODE_TYPE_DECL:
-                return (node->type >= IDL_AST_NODE_TYPE_DECL && node->type < IDL_AST_NODE_TYPE_DECL_REF) ||
-                       node->type >= IDL_AST_NODE_TYPE_TYPE;
+                return (nodeType >= IDL_AST_NODE_TYPE_DECL && nodeType < IDL_AST_NODE_TYPE_DECL_REF) ||
+                       nodeType >= IDL_AST_NODE_TYPE_TYPE && nodeType <= IDL_AST_NODE_TYPE_FLOAT_64;
             case IDL_AST_NODE_TYPE_LITERAL:
-                return node->type >= IDL_AST_NODE_TYPE_LITERAL && node->type < IDL_AST_NODE_TYPE_ATTR;
+                return nodeType >= IDL_AST_NODE_TYPE_LITERAL && nodeType < IDL_AST_NODE_TYPE_ATTR;
             case IDL_AST_NODE_TYPE_ATTR:
-                return node->type >= IDL_AST_NODE_TYPE_ATTR && node->type < IDL_AST_NODE_TYPE_TYPE;
+                return nodeType >= IDL_AST_NODE_TYPE_ATTR && nodeType < IDL_AST_NODE_TYPE_TYPE;
             case IDL_AST_NODE_TYPE_ATTR_DOC:
-                return node->type >= IDL_AST_NODE_TYPE_ATTR_DOC && node->type < IDL_AST_NODE_TYPE_TYPE;
+                return nodeType >= IDL_AST_NODE_TYPE_ATTR_DOC && nodeType < IDL_AST_NODE_TYPE_TYPE;
             case IDL_AST_NODE_TYPE_TYPE:
-                return node->type >= IDL_AST_NODE_TYPE_TYPE || node->type == IDL_AST_NODE_TYPE_ENUM ||
-                       node->type == IDL_AST_NODE_TYPE_STRUCT;
+                return nodeType >= IDL_AST_NODE_TYPE_TYPE && nodeType <= IDL_AST_NODE_TYPE_FLOAT_64 ||
+                       nodeType == IDL_AST_NODE_TYPE_ENUM || nodeType == IDL_AST_NODE_TYPE_STRUCT;
             case IDL_AST_NODE_TYPE_TRIVIAL_TYPE:
-                return node->type >= IDL_AST_NODE_TYPE_TRIVIAL_TYPE;
+                return nodeType >= IDL_AST_NODE_TYPE_TRIVIAL_TYPE && nodeType <= IDL_AST_NODE_TYPE_FLOAT_64;
             case IDL_AST_NODE_TYPE_INTEGER_TYPE:
-                return node->type >= IDL_AST_NODE_TYPE_INTEGER_TYPE && node->type < IDL_AST_NODE_TYPE_FLOAT_TYPE;
+                return nodeType >= IDL_AST_NODE_TYPE_INTEGER_TYPE && nodeType < IDL_AST_NODE_TYPE_FLOAT_TYPE;
             case IDL_AST_NODE_TYPE_FLOAT_TYPE:
-                return node->type >= IDL_AST_NODE_TYPE_FLOAT_TYPE;
+                return nodeType >= IDL_AST_NODE_TYPE_FLOAT_TYPE && nodeType <= IDL_AST_NODE_TYPE_FLOAT_64;
             default:
                 break;
         }

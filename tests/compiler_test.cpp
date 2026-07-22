8eb48a302c9c95047e68ef4b4df7e73ecce5c7ee
Vladimir Shaleev <vladimirshaleev@gmail.com>
2026-07-22 14:48:23 +0300

    Add test case e3053 for field

--- tests/compiler_test.cpp
+++ tests/compiler_test.cpp
@@ -1717,9 +1717,23 @@ TEST(idlc, ArrayAttrMustReferToIntField) {
 }
 
 TEST(idlc, RefToTheArraySizeMustBeInTheSameScope) {
-    const auto [result, ast, messages] = compile("e3053");
+    const auto [result, ast, messages] = compile("e3053field");
     deferred(idl_compilation_result_destroy(ast));
-    GTEST_FAIL();
+    ASSERT_EQ(result, IDL_RESULT_SUCCESS);
+    ASSERT_EQ(messages.size(), 1);
+    ASSERT_EQ(messages[0],
+              "error [E3053]: The reference to the array size must be in the same scope as field 'Api.Test.Arr' at "
+              "e3053field:16:5");
+
+    auto api = idl_compilation_result_get_api(ast);
+    ASSERT_NE(api, HandleNone);
+
+    auto test = findChild(ast, api, "Test");
+    ASSERT_NE(test, HandleNone);
+
+    auto fields = getChilds(ast, test, IDL_AST_NODE_TYPE_FIELD);
+    ASSERT_EQ(fields.size(), 1);
+    ASSERT_TRUE(hasAnyState(ast, fields[0], IDL_AST_NODE_STATE_BUILD_ERROR_BIT));
 }
 
 TEST(idlc, ArraySizeRefCannotReferToItsOwnDeclaration) {
